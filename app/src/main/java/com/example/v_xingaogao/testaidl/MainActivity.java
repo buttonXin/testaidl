27ceed8590d93f42c6c89510e7245408543fd16d
xingaogao <xingaogao@baidu.com>
2018-11-09 17:36:14 +0800

    学习aidl 第一次提交

---
+++ app/src/main/java/com/example/v_xingaogao/testaidl/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.v_xingaogao.testaidl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
